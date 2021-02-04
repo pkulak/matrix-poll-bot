e287d2c6773e0abd56a7ca6b215e1f963575af86
Phil <pkulak@hey.com>
2021-02-04 21:24:04 +0000

    Don't try to take emojiis from the line; use defaults.

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"regexp"
 	"strings"
 	"time"
 
@@ -16,8 +15,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// emojiRegexpStr is the regexp for detecting emojis (as a string). Copied from https://github.com/urakozz/go-emoji
-const emojiRegexpStr = "[\\x{2712}\\x{2714}\\x{2716}\\x{271d}\\x{2721}\\x{2728}\\x{2733}\\x{2734}\\x{2744}\\x{2747}\\x{274c}\\x{274e}\\x{2753}-\\x{2755}\\x{2757}\\x{2763}\\x{2764}\\x{2795}-\\x{2797}\\x{27a1}\\x{27b0}\\x{27bf}\\x{2934}\\x{2935}\\x{2b05}-\\x{2b07}\\x{2b1b}\\x{2b1c}\\x{2b50}\\x{2b55}\\x{3030}\\x{303d}\\x{1f004}\\x{1f0cf}\\x{1f170}\\x{1f171}\\x{1f17e}\\x{1f17f}\\x{1f18e}\\x{1f191}-\\x{1f19a}\\x{1f201}\\x{1f202}\\x{1f21a}\\x{1f22f}\\x{1f232}-\\x{1f23a}\\x{1f250}\\x{1f251}\\x{1f300}-\\x{1f321}\\x{1f324}-\\x{1f393}\\x{1f396}\\x{1f397}\\x{1f399}-\\x{1f39b}\\x{1f39e}-\\x{1f3f0}\\x{1f3f3}-\\x{1f3f5}\\x{1f3f7}-\\x{1f4fd}\\x{1f4ff}-\\x{1f53d}\\x{1f549}-\\x{1f54e}\\x{1f550}-\\x{1f567}\\x{1f56f}\\x{1f570}\\x{1f573}-\\x{1f579}\\x{1f587}\\x{1f58a}-\\x{1f58d}\\x{1f590}\\x{1f595}\\x{1f596}\\x{1f5a5}\\x{1f5a8}\\x{1f5b1}\\x{1f5b2}\\x{1f5bc}\\x{1f5c2}-\\x{1f5c4}\\x{1f5d1}-\\x{1f5d3}\\x{1f5dc}-\\x{1f5de}\\x{1f5e1}\\x{1f5e3}\\x{1f5ef}\\x{1f5f3}\\x{1f5fa}-\\x{1f64f}\\x{1f680}-\\x{1f6c5}\\x{1f6cb}-\\x{1f6d0}\\x{1f6e0}-\\x{1f6e5}\\x{1f6e9}\\x{1f6eb}\\x{1f6ec}\\x{1f6f0}\\x{1f6f3}\\x{1f910}-\\x{1f918}\\x{1f980}-\\x{1f984}\\x{1f9c0}\\x{3297}\\x{3299}\\x{a9}\\x{ae}\\x{203c}\\x{2049}\\x{2122}\\x{2139}\\x{2194}-\\x{2199}\\x{21a9}\\x{21aa}\\x{231a}\\x{231b}\\x{2328}\\x{2388}\\x{23cf}\\x{23e9}-\\x{23f3}\\x{23f8}-\\x{23fa}\\x{24c2}\\x{25aa}\\x{25ab}\\x{25b6}\\x{25c0}\\x{25fb}-\\x{25fe}\\x{2600}-\\x{2604}\\x{260e}\\x{2611}\\x{2614}\\x{2615}\\x{2618}\\x{261d}\\x{2620}\\x{2622}\\x{2623}\\x{2626}\\x{262a}\\x{262e}\\x{262f}\\x{2638}-\\x{263a}\\x{2648}-\\x{2653}\\x{2660}\\x{2663}\\x{2665}\\x{2666}\\x{2668}\\x{267b}\\x{267f}\\x{2692}-\\x{2694}\\x{2696}\\x{2697}\\x{2699}\\x{269b}\\x{269c}\\x{26a0}\\x{26a1}\\x{26aa}\\x{26ab}\\x{26b0}\\x{26b1}\\x{26bd}\\x{26be}\\x{26c4}\\x{26c5}\\x{26c8}\\x{26ce}\\x{26cf}\\x{26d1}\\x{26d3}\\x{26d4}\\x{26e9}\\x{26ea}\\x{26f0}-\\x{26f5}\\x{26f7}-\\x{26fa}\\x{26fd}\\x{2702}\\x{2705}\\x{2708}-\\x{270d}\\x{270f}]|\\x{23}\\x{20e3}|\\x{2a}\\x{20e3}|\\x{30}\\x{20e3}|\\x{31}\\x{20e3}|\\x{32}\\x{20e3}|\\x{33}\\x{20e3}|\\x{34}\\x{20e3}|\\x{35}\\x{20e3}|\\x{36}\\x{20e3}|\\x{37}\\x{20e3}|\\x{38}\\x{20e3}|\\x{39}\\x{20e3}|\\x{1f1e6}[\\x{1f1e8}-\\x{1f1ec}\\x{1f1ee}\\x{1f1f1}\\x{1f1f2}\\x{1f1f4}\\x{1f1f6}-\\x{1f1fa}\\x{1f1fc}\\x{1f1fd}\\x{1f1ff}]|\\x{1f1e7}[\\x{1f1e6}\\x{1f1e7}\\x{1f1e9}-\\x{1f1ef}\\x{1f1f1}-\\x{1f1f4}\\x{1f1f6}-\\x{1f1f9}\\x{1f1fb}\\x{1f1fc}\\x{1f1fe}\\x{1f1ff}]|\\x{1f1e8}[\\x{1f1e6}\\x{1f1e8}\\x{1f1e9}\\x{1f1eb}-\\x{1f1ee}\\x{1f1f0}-\\x{1f1f5}\\x{1f1f7}\\x{1f1fa}-\\x{1f1ff}]|\\x{1f1e9}[\\x{1f1ea}\\x{1f1ec}\\x{1f1ef}\\x{1f1f0}\\x{1f1f2}\\x{1f1f4}\\x{1f1ff}]|\\x{1f1ea}[\\x{1f1e6}\\x{1f1e8}\\x{1f1ea}\\x{1f1ec}\\x{1f1ed}\\x{1f1f7}-\\x{1f1fa}]|\\x{1f1eb}[\\x{1f1ee}-\\x{1f1f0}\\x{1f1f2}\\x{1f1f4}\\x{1f1f7}]|\\x{1f1ec}[\\x{1f1e6}\\x{1f1e7}\\x{1f1e9}-\\x{1f1ee}\\x{1f1f1}-\\x{1f1f3}\\x{1f1f5}-\\x{1f1fa}\\x{1f1fc}\\x{1f1fe}]|\\x{1f1ed}[\\x{1f1f0}\\x{1f1f2}\\x{1f1f3}\\x{1f1f7}\\x{1f1f9}\\x{1f1fa}]|\\x{1f1ee}[\\x{1f1e8}-\\x{1f1ea}\\x{1f1f1}-\\x{1f1f4}\\x{1f1f6}-\\x{1f1f9}]|\\x{1f1ef}[\\x{1f1ea}\\x{1f1f2}\\x{1f1f4}\\x{1f1f5}]|\\x{1f1f0}[\\x{1f1ea}\\x{1f1ec}-\\x{1f1ee}\\x{1f1f2}\\x{1f1f3}\\x{1f1f5}\\x{1f1f7}\\x{1f1fc}\\x{1f1fe}\\x{1f1ff}]|\\x{1f1f1}[\\x{1f1e6}-\\x{1f1e8}\\x{1f1ee}\\x{1f1f0}\\x{1f1f7}-\\x{1f1fb}\\x{1f1fe}]|\\x{1f1f2}[\\x{1f1e6}\\x{1f1e8}-\\x{1f1ed}\\x{1f1f0}-\\x{1f1ff}]|\\x{1f1f3}[\\x{1f1e6}\\x{1f1e8}\\x{1f1ea}-\\x{1f1ec}\\x{1f1ee}\\x{1f1f1}\\x{1f1f4}\\x{1f1f5}\\x{1f1f7}\\x{1f1fa}\\x{1f1ff}]|\\x{1f1f4}\\x{1f1f2}|\\x{1f1f5}[\\x{1f1e6}\\x{1f1ea}-\\x{1f1ed}\\x{1f1f0}-\\x{1f1f3}\\x{1f1f7}-\\x{1f1f9}\\x{1f1fc}\\x{1f1fe}]|\\x{1f1f6}\\x{1f1e6}|\\x{1f1f7}[\\x{1f1ea}\\x{1f1f4}\\x{1f1f8}\\x{1f1fa}\\x{1f1fc}]|\\x{1f1f8}[\\x{1f1e6}-\\x{1f1ea}\\x{1f1ec}-\\x{1f1f4}\\x{1f1f7}-\\x{1f1f9}\\x{1f1fb}\\x{1f1fd}-\\x{1f1ff}]|\\x{1f1f9}[\\x{1f1e6}\\x{1f1e8}\\x{1f1e9}\\x{1f1eb}-\\x{1f1ed}\\x{1f1ef}-\\x{1f1f4}\\x{1f1f7}\\x{1f1f9}\\x{1f1fb}\\x{1f1fc}\\x{1f1ff}]|\\x{1f1fa}[\\x{1f1e6}\\x{1f1ec}\\x{1f1f2}\\x{1f1f8}\\x{1f1fe}\\x{1f1ff}]|\\x{1f1fb}[\\x{1f1e6}\\x{1f1e8}\\x{1f1ea}\\x{1f1ec}\\x{1f1ee}\\x{1f1f3}\\x{1f1fa}]|\\x{1f1fc}[\\x{1f1eb}\\x{1f1f8}]|\\x{1f1fd}\\x{1f1f0}|\\x{1f1fe}[\\x{1f1ea}\\x{1f1f9}]|\\x{1f1ff}[\\x{1f1e6}\\x{1f1f2}\\x{1f1fc}]"
+var bullets = []string{"1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"}
 
 // config is the configuration structure.
 type config struct {
@@ -40,25 +38,13 @@ type reaction struct {
 
 // handler defines the handlers to call when processing incoming Matrix events, along with some util functions.
 type handler struct {
-	Client               *gomatrix.Client
-	EmojiRegexp          *regexp.Regexp
-	StartingSpacesRegexp *regexp.Regexp
+	Client *gomatrix.Client
 }
 
 func newHandler(homeserverURL, userID, accessToken string) (h *handler, err error) {
 	h = new(handler)
 
 	h.Client, err = gomatrix.NewClient(homeserverURL, userID, accessToken)
-	if err != nil {
-		return
-	}
-
-	h.EmojiRegexp, err = regexp.Compile(emojiRegexpStr)
-	if err != nil {
-		return
-	}
-
-	h.StartingSpacesRegexp, err = regexp.Compile("^ +")
 	if err != nil {
 		return
 	}
@@ -139,10 +125,10 @@ func (h *handler) handleMessage(event *gomatrix.Event) {
 	}
 
 	// Add reactions to the poll message.
-	for k := range choices {
+	for index, _ := range choices {
 		content := reaction{}
 		content.RelatesTo.RelType = "m.annotation"
-		content.RelatesTo.Key = k
+		content.RelatesTo.Key = bullets[index]
 		content.RelatesTo.EventID = res.EventID
 
 		if _, err := h.Client.SendMessageEvent(event.RoomID, "m.reaction", &content); err != nil {
@@ -152,19 +138,27 @@ func (h *handler) handleMessage(event *gomatrix.Event) {
 			return
 		}
 	}
+
+	// Redact the original message.
+	if _, err := h.Client.RedactEvent(event.RoomID, event.ID, &gomatrix.ReqRedact{Reason: "poll"}); err != nil {
+		logger.WithField("poll_event_id", res.EventID).Error(
+			"Couldn't redact message. Maybe set to power level higher?",
+		)
+		return
+	}
+
 }
 
 // parseMessage extracts the question and choices from the given string.
 // Returns with the question and choices, and an error string to send to the room if the message isn't formatted
 // correctly.
-func (h *handler) parseMessage(body string) (question string, choices map[string]string, errMsg string) {
+func (h *handler) parseMessage(body string) (question string, choices []string, errMsg string) {
 	r := strings.NewReader(body)
 
 	scanner := bufio.NewScanner(r)
 
-	choices = make(map[string]string)
+	choices = make([]string, 0)
 	firstLine := true
-	choiceErrMsg := "Each choice needs to be on a different line, and to start with an emoji."
 	// Read the message line by line.
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -178,35 +172,12 @@ func (h *handler) parseMessage(body string) (question string, choices map[string
 				return
 			}
 		} else {
-			// handle empty lines and lines containing only spaces.
-			// TODO: also remove tabs.
-			if len(h.trimStartingSpaces(line)) == 0 {
+			// handle empty lines
+			if len(line) < 3 {
 				continue
 			}
 
-			// This is a choice in the poll.
-			indexes := h.EmojiRegexp.FindStringIndex(line)
-			if len(indexes) > 0 {
-				// The line must start with an emoji.
-				if indexes[0] != 0 {
-					errMsg = choiceErrMsg
-					return
-				}
-
-				key := line[:indexes[1]]
-				// Allow each emoji only once.
-				if _, exists := choices[key]; exists {
-					errMsg = fmt.Sprintf("Two occurrences of emoji %s, please use each emoji only once.", key)
-				}
-
-				// Save the choice.
-				value := line[indexes[1]:]
-				choices[key] = h.trimStartingSpaces(value)
-			} else {
-				// Each choice line must contain an emoji.
-				errMsg = choiceErrMsg
-				return
-			}
+			choices = append(choices, line)
 		}
 
 		firstLine = false
@@ -215,18 +186,8 @@ func (h *handler) parseMessage(body string) (question string, choices map[string
 	return
 }
 
-// trimStartingSpaces removes the spaces at the beginning of a string.
-func (h *handler) trimStartingSpaces(s string) string {
-	r := regexp.MustCompile("^ +")
-	indexes := r.FindStringIndex(s)
-	if len(indexes) == 0 {
-		return s
-	}
-	return s[indexes[1]:]
-}
-
 // generateNoticeHTML generates the HTML text to send as a notice to the room.
-func (h *handler) generateNoticeHTML(userID string, question string, choices map[string]string) string {
+func (h *handler) generateNoticeHTML(userID string, question string, choices []string) string {
 	displayName := userID
 
 	// Try to retrieve the sender's display name from the homeserver. If we can't, use the user ID instead.
@@ -244,8 +205,8 @@ func (h *handler) generateNoticeHTML(userID string, question string, choices map
 
 	notice := fmt.Sprintf(format, userID, displayName, question)
 
-	for key, value := range choices {
-		notice += fmt.Sprintf("%s: %s<br>", key, value)
+	for index, value := range choices {
+		notice += fmt.Sprintf("%s: %s<br>", bullets[index], value)
 	}
 
 	return notice
